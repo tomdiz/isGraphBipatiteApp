4802b0682f189b4cbc88b771a15778040257adb1
Thomas DiZoglio <tomdiz@yahoo.com>
2023-01-02 09:53:31 -0500

    added more comments to graphdata model

--- isGraphBipatite/Models/GraphData.swift
+++ isGraphBipatite/Models/GraphData.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 /*
- Depth First Search Algorithm using 2 colors to figure out if bipartite graph.
+ Depth First Search Algorithm using two-colorability to figure out if it is a bipartite graph.
  
  Use two colors to color the graph and see if there are any adjacent nodes having the same color.
  
@@ -22,6 +22,11 @@ import Foundation
      1. If color not set yet, color it. Then use the other color to color all its adjacent nodes (DFS).
      2. If it has been colored, check if the current color is the same as the color that is going to be used to color it.
 
+ This is an O(N) - Try to color all nodes with alternating colors. If we came back to an already-colored node with a different color, then we return false. It could visit all nodes at least once making it O(N). Accessing each node in O(1)
+ 
+ Basic DFS using Stack algorithm below:
+ 
+ The depth-first search or DFS algorithm traverses or explores data structures, such as trees and graphs. The algorithm starts at the root node (in the case of a graph, you can use any random node as the root node) and examines each branch as far as possible before backtracking. When a dead-end occurs in any iteration, the Depth First Search (DFS) method traverses a network in a deathward motion and uses a stack data structure to remember to acquire the next vertex to start a search.
  */
 
 struct GraphData: Codable, Hashable, Identifiable {
